9f0cd4cc889d604f8a77c43534c890d526387233
Dan-Ant <daniele.antonacci@marconibari.edu.it>
2025-09-25 07:39:53 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,38 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int n,i,j,t;
+cout<<"Quante cifre vuoi?" << endl;
+cin>>n;
+<vector> int v[n];
+i=0;
+while (i<n)
+{
+   v[i]=rand ()n*10;
+   i=i+1;
+}
+i=0;
+t=0;
+while (i<n)
+{
+   j=0;
+   while (j<=n-2)
+   {
+    if (v[j]>v[j+1])
+    {
+       t=v[j];
+       v[j]=v[j+1];
+       v[j+1]=t;
+    }
+    j=j+1;
+    
+   }
+   cout<<v[i]<<endl;
+   i=i+1;
+   
 }
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+
+}
+
+
